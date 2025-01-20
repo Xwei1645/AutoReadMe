6722af3f1d100fa1a099d0ec2ed529a286ea1d73
Xwei1645 <xuwei1645@163.com>
2025-01-20 18:31:49 +0800

    fix: 修改插件设置界面

--- AutoReadMe/Views/SettingsPages/SettingsPage.xaml.cs
+++ AutoReadMe/Views/SettingsPages/SettingsPage.xaml.cs
@@ -1,21 +1,6 @@
 ﻿using ClassIsland.Core.Abstractions.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ClassIsland.Core.Attributes;
 using MaterialDesignThemes.Wpf;
-using AutoReadMe;
 
 namespace AutoReadMe.Views.SettingsPages;
 
@@ -23,11 +8,11 @@ namespace AutoReadMe.Views.SettingsPages;
 /// ExampleSettingsPage.xaml 的交互逻辑
 /// </summary>
 [SettingsPageInfo("Xwei1645.AutoReadMe", "AutoReadMe", PackIconKind.CogOutline, PackIconKind.Cog)]
-public partial class ExampleSettingsPage : SettingsPageBase
+public partial class SettingsPage : SettingsPageBase
 {
     public Plugin Plugin { get; }
 
-    public ExampleSettingsPage(Plugin plugin)
+    public SettingsPage(Plugin plugin)
     {
         Plugin = plugin;
         InitializeComponent();
